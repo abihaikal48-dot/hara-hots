2ccd967bc67c5d73e91d2f32a2fd4de384004800
abihaikal48-dot <abihaikal48@gmail.com>
2026-07-02 17:23:44 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        deep: "#070101",
-        surface: "#120303",
-        elevated: "#1B0606",
-        crimson: "#D42B2B",
-        gold: "#F5C518",
-      },
+        hotsDeep: '#070101',
+        hotsSurface: '#120303',
+        hotsElevated: '#1B0606',
+        hotsCrimson: '#D42B2B',
+        hotsGold: '#F5C518',
+        piketHijau: '#064E3B',
+        piketCyan: '#083344',
+        piketLavender: '#2E1065',
+        piketBiru: '#1E3A8A',
+        piketOrange: '#7C2D12',
+        piketGray: '#1F2937',
+      }
     },
   },
   plugins: [],
